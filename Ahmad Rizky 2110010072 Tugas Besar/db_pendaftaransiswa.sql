f265ddf50157272284db33e3d86a114262f8c46c
Ahmad Rizky <134019247+AhmadRizky2110010072@users.noreply.github.com>
2024-01-07 13:24:22 +0800

    Add files via upload

---
+++ Ahmad Rizky 2110010072 Tugas Besar/db_pendaftaransiswa.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2024 at 06:09 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pendaftaransiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `idpendaftaran` int(11) NOT NULL,
+  `nis` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `jeniskelamin` varchar(250) NOT NULL,
+  `alamat` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pendaftaran`
+--
+
+INSERT INTO `pendaftaran` (`idpendaftaran`, `nis`, `nama`, `jeniskelamin`, `alamat`) VALUES
+(1, 456, 'Ahmad Rizky', 'Laki-Laki', 'Handil'),
+(2, 34, 'Ciya', 'Perempuan', 'Kayu Tangi'),
+(3, 55, 'Irfansyah', 'Laki-Laki', 'Kelayan'),
+(5, 122, 'Reza', 'Laki-Laki', 'Veteran');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `jeniskelamin` varchar(250) NOT NULL,
+  `asalsekolah` varchar(250) NOT NULL,
+  `alamat` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nis`, `nama`, `jeniskelamin`, `asalsekolah`, `alamat`) VALUES
+(2, 'Rizky', 'Laki-Laki', 'SMA 2', 'Kayu Tangi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `idpembayaran` int(11) NOT NULL,
+  `nis` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `biayamasuk` varchar(250) NOT NULL,
+  `uangpangkal` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`idpembayaran`, `nis`, `nama`, `biayamasuk`, `uangpangkal`) VALUES
+(123, 22, 'Ahmad Rizky', '350000', '2500000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`idpendaftaran`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`idpembayaran`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  MODIFY `idpendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24345;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `nis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=234;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `idpembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1235;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
